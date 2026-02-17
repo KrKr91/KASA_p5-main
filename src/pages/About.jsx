e9a379eab36742e2465f8c9c021fc25f1948d47d
KrKr91 <shelron.krkr@gmail.com>
2026-02-17 14:01:13 +0100

    Add About.css for styling the About section layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,17 @@
+import Banner from '../components/Banner'
+import imgAboutBanner from '../assets/about-banner.png'
+import '../styles/About.css'
+
 function About() {
-    return <h1>A Propos </h1>
+    return (
+        <div className="about">
+            <Banner image={imgAboutBanner} />
+            
+            <div className="about__container">
+                <h1>a propos</h1>
+            </div>
+        </div>
+    )
 }
+
 export default About
