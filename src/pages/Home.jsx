41dd398069331df2da923416090410989ff837ea
KrKr91 <shelron.krkr@gmail.com>
2026-02-17 12:32:23 +0100

    Add Banner and Card components with associated styles
    - Created Banner component to display an image with overlay and text.
    - Added Banner.scss for styling the Banner component.
    - Created Card component structure.
    - Added Home.scss for styling the home gallery layout.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
+import Banner from '../components/Banner'
+import Card from '../components/Card'
+import logements from '../data/logements.json' 
+import imgHomeBanner from '../assets/home-banner.png'
+import '../styles/Home.scss' 
+
 function Home() {
     return (
-        <div>
-            <h1>Page d'Accueil </h1>
+        <div className="home">
+            <Banner image={imgHomeBanner} text="Chez vous, partout et ailleurs" />
+            
+            <div className="home__gallery">
+                {logements.map((logement) => (
+                    <Card 
+                        key={logement.id} 
+                        id={logement.id}
+                        title={logement.title}
+                        cover={logement.cover}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
+
 export default Home
